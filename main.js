55dd591f8e405df8af6c5db4e8d5fdb0b86c2d27
jeanvitor10 <111467996+jeanvitor10@users.noreply.github.com>
2024-03-11 10:12:14 -0300

    lk

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const botoes = document.querySelectorAll(".botao");
 
-for(let i=0;i <botoes.length;++)}
+for(let i=0;i <botoes.length;++){  
   botoes[i].inclick = function(){
     for(let j=0;<botoes.length;j++){
         botoes[j].classList.remove("ativo");
