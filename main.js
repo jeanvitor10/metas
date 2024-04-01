b0198ed874b3f5fa200b4d37c14ad6e0f163c5fe
jeanvitor10 <111467996+jeanvitor10@users.noreply.github.com>
2024-04-01 10:15:52 -0300

    jjjjjjk

--- main.js
+++ main.js
@@ -1,10 +1,23 @@
 const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo")
 
 for(let i=0;i <botoes.length;++){  
   botoes[i].inclick = function(){
     for(let j=0;<botoes.length;j++){
         botoes[j].classList.remove("ativo");
+        textos[j].classList.remove('ativo')
     }
     botoes[i].classList.add("ativo");
+    textos[i].classList.add("ativo");
   }
 }
+const contador = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new date("2024-12-21t00:00:00");
+let tempoAtual = new date();
+contadores[0].textoContent = tempoAtual;  
+
+let tempofinal = tempoOjetivol - tempoAtual;
+let segundo = tempofinal1/1000;
+let minutos = segundo/60;
+let horas = minutos/60;
+let dias = horas/24;
